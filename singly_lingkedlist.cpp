d06317d49d7ab0d4a78d91668b587d63f59d9cb7
dwirahmanvalensa-del <dwirahmanvalensa@gmail.com>
2026-04-24 16:51:07 +0800

    logika mencari posisi insert tengah atau akhir

--- singly_lingkedlist.cpp
+++ singly_lingkedlist.cpp
@@ -39,4 +39,18 @@ class LinkedList
             return;
         }
 
+        Node *previous = START;
+        Node *current = START;
+
+        while ((current != NULL) && (nim >= current->noMhs))
+        {
+            if (nim == current->noMhs)
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+            previous = current;
+            current = current->next;
+        }
+
         
